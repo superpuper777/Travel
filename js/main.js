aecbcdd6bf7b02a4720b77f76dd38b0c6c8b0d48
superpuper777 <superfevrik@gmail.com>
2020-05-15 15:05:34 +0300

    Make layout adaptive

--- js/main.js
+++ js/main.js
@@ -8,3 +8,17 @@
     }
   }
 }());
+
+//Burger Handler 
+
+(function () {
+  const burgerItem = document.querySelector('.burger');
+  const menu = document.querySelector('.header__nav');
+  const esc = document.querySelector('.header__nav-close');
+  burgerItem.addEventListener('click', () => {
+    menu.classList.add('header__nav_active');
+  });
+  esc.addEventListener('click', () => {
+    menu.classList.remove('header__nav_active');
+  });
+}());
